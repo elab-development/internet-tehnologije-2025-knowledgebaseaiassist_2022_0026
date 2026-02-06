4a8391498186b045256135f43bae7fc2d1fe49b3
tricetvrt <nzmaleksa@gmail.com>
2026-02-06 02:32:18 +0100

    initial django setup + serializers
    set up the django backend folder structure, some initial settings, update the requirements.txt, add UserSerializer to implement user registration/login

---
+++ backend/api/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+# prihvatamo json podatke i vracamo json podatke frontendu
+# serializer pajton objekte transformise u json i obrnuto
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: # definisemo sta pakujemo u json
+        model = User
+        fields = ["id","username","password","first_name","last_name"] # mozemo da dodamo email
+        extra_kwargs = {"password":{"write_only":True}} # prihvatamo sifru ali ne vracamo kao podatak
+        # django apparently automatski hendluje id
+
+    def create(self,provereni_podaci):
+        user = User.objects.create_user(**provereni_podaci)
+        # create_user hashuje sifru u bazi
+        return user
